5648eb8bcf7f0411f95acc5b9931b89a58c98155
慧科云 <geek@13aq.com>
2023-04-08 09:10:09 +0800

    去掉未使用

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,10 @@
-/*******************************************************************************
- * Copyright (c) 2023 慧科云
- * 此源代码根据 MIT 许可证进行许可，该许可证位于此源代码树根目录中的 LICENSE 文件中。
- * 最后修改时间：2023/4/8 上午9:05
- ******************************************************************************/
-
 import React from 'react'
 import Layout from '@theme/Layout'
 
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import Hero from '@site/src/components/Hero'
 import BackToTopButton from '@theme/BackToTopButton'
 
 export default function Hello() {
-  const context = useDocusaurusContext()
-  const { siteConfig = {} } = context
   return (
     <Layout wrapperClassName="blog=-list__page">
       <Hero />
